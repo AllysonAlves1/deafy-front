36ca8f7e864e3bfdae4c6822d728b19c336fbfff
giovaniflp <giovanifeitosa123@gmail.com>
2023-11-07 22:14:54 -0300

    Alteração da landing page, adição de responsividade e estilização

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,20 @@
 import Link from "next/link";
 import RootLayout from "./layout";
 
+
 export default function home() {
 
   const layoutProps = false;
 
   return (
     <RootLayout layoutProps={layoutProps}>
-      <div className="flex flex-col items-center justify-between pr-25 pl-25 pt-25 pb-25 h-96">
-        <h1 className="text-6xl font-bold">Deafy Project</h1>
-        <p>Bem-vindo ao nosso aplicativo de música voltada à inclusão!</p>
-        <Link href='/login' className="bg-white text-black rounded-lg px-4 py-2">Página de Login</Link>
-        <Link href='/register' className="bg-white text-black rounded-lg px-4 py-2">Página de Registro</Link>
-        <footer>Coders: Allyson Alves, Giovani Feitosa, Lucas Emanuel.</footer>
-      </div>
+        <div className="flex flex-col items-center justify-center pr-25 pl-25 pt-25 pb-25 gap-5 h-screen">
+          <h1 className="text-6xl font-bold text-center">Deafy Project</h1>
+          <p className="text-sm text-center">Bem-vindo ao nosso aplicativo de música voltada à inclusão!</p>
+          <Link href='/login' className="bg-white text-black rounded-lg px-4 py-2">Página de Login</Link>
+          <Link href='/register' className="bg-white text-black rounded-lg px-4 py-2">Página de Registro</Link>
+          <footer className="text-sm text-center">Coders: Allyson Alves, Giovani Feitosa, Lucas Emanuel.</footer>
+        </div>
     </RootLayout>
   );
 }
