a404fb26d23e466bfcb64f2a5f42d6ac93295db9
giovaniflp <giovanifeitosa123@gmail.com>
2023-11-08 14:22:13 -0300

    Alterações nas páginas profile, search e em seus respectivos components

--- src/app/components/searchbar/searchBar.tsx
+++ src/app/components/searchbar/searchBar.tsx
@@ -9,7 +9,7 @@ export default function SearchBar() {
       </span>
       <input
         className="placeholder:italic placeholder:text-slate-400 text-slate-400 block bg-white w-full border border-slate-300 rounded-md py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm"
-        placeholder="Search for music..."
+        placeholder="Search for music, podcast, author..."
         type="text"
         name="search"
       />
