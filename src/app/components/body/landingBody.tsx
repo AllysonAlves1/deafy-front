b825fffb7071d15049d4c345e25bd077ad903d6a
Queriasup <lucasemanuel.adn@gmail.com>
2023-11-27 05:26:30 -0300

    estilizacao landing page

--- src/app/components/body/landingBody.tsx
+++ src/app/components/body/landingBody.tsx
@@ -7,6 +7,7 @@ import menubars from "../../../../public/menubars.png"
 import closebutton from "../../../../public/closebutton.webp"
 
 
+
 export default function LandingBody(){
     const [isOpen, setIsOpen] = useState(false);
 
@@ -38,14 +39,20 @@ export default function LandingBody(){
                     onClick={alteraEstado}
                 />
             </div>
-            <div className={`${isOpen? "w-screen" : "hidden"} h-screen bg-slate-800`}>
-
-            </div>
-            <div className="flex flex-col items-center justify-center pr-25 pl-25 pt-25 pb-25 gap-5 h-screen">
-                <h1 className="text-6xl font-bold text-center">Deafy Project</h1>
-                <p className="text-sm text-center">Bem-vindo ao nosso aplicativo de música voltada à inclusão!</p>
-                <Link href='/login' className="bg-white text-black rounded-lg px-4 py-2">Página de Login</Link>
+            <div className={`${isOpen? "w-screen overflow-hidden" : "hidden"} flex flex-col justify-center items-center gap-2 h-screen bg-slate-800`}>
+                <Link href='/login' className=" text-black text-xl">Login</Link>
                 <Link href='/register' className="bg-white text-black rounded-lg px-4 py-2">Página de Registro</Link>
+            </div>
+            <div className={`${isOpen? "hidden" : "w-screen"} flex flex-col h-screen bg-slate-800`}>
+                <div className="bg-landingImage bg-center bg-cover bg-blend-darken bg-no-repeat h-96 w-full">
+                    <div className="flex flex-col justify-center items-center gap-2 w-full h-full backdrop-blur-sm">
+                        <h1 className="text-white text-6xl font-bold text-center drop-shadow-md">Deafy </h1>
+                        <p className="text-white text-base text-center drop-shadow-md">Bem-vindo ao nosso aplicativo de música voltado à inclusão!</p>
+                    </div>
+                </div>
+                <div className="bg-white h-screen">
+
+                </div>
                 <footer className="text-sm text-center">Coders: Allyson Alves, Giovani Feitosa, Lucas Emanuel.</footer>
             </div>
         </div>
